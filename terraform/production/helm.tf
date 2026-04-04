723342b7403b4ca98a1af168ff71805292889a64
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2026-04-04 19:30:24 -0300

    feat: adds initial structure for argocd

---
+++ terraform/production/helm.tf
@@ -0,0 +1,28 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "5.51.6"
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  depends_on = [module.eks]
+}
