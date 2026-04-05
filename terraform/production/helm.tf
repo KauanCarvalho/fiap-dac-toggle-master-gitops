e25aabddfe9bae195f840da4b912389750610957
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2026-04-05 12:56:24 -0300

    feat: adds metrics server for k8s and hpas

--- terraform/production/helm.tf
+++ terraform/production/helm.tf
@@ -31,3 +31,23 @@ resource "helm_release" "argocd" {
 
   depends_on = [module.eks]
 }
+
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
+
+  set {
+    name  = "args[0]"
+    value = "--kubelet-insecure-tls"
+  }
+
+  set {
+    name  = "args[1]"
+    value = "--kubelet-preferred-address-types=InternalIP"
+  }
+
+  depends_on = [module.eks]
+}
